6ff9d0371f586a289e3739450b069ac07b57f272
Milen03 <milen.atanasovv03@gmail.com>
2026-05-15 19:37:04 +0300

    List all notes

--- client/src/components/NotesSection.jsx
+++ client/src/components/NotesSection.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react"
+import nodeServices from "../services/nodeServices"
+import NoteCart from "./NoteCart"
+
 export default function NotesSection(){
+
+    const [notes,setNotes] = useState([])
+
+    useEffect(()=>{
+        nodeServices.getAll()
+        .then(result =>{
+            setNotes(result)
+        })
+    },[])
 
     return (
         <>
@@ -10,67 +23,11 @@ export default function NotesSection(){
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
           {/* Note Card */}
-          <div className="bg-white border border-zinc-300 p-5 rounded-2xl shadow-md">
-            <h3 className="text-xl font-semibold mb-4">
-              React Notes
-            </h3>
+         {notes.map(note => <NoteCart
+         key={note._id}
+         {...note}
+         />)}
 
-            <div className="flex gap-3">
-              <button className="flex-1 bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition">
-                View
-              </button>
-
-              <button className="flex-1 bg-yellow-500 text-white py-2 rounded-xl hover:bg-yellow-600 transition">
-                Edit
-              </button>
-
-              <button className="flex-1 bg-red-500 text-white py-2 rounded-xl hover:bg-red-600 transition">
-                Delete
-              </button>
-            </div>
-          </div>
-
-          {/* Note Card */}
-          <div className="bg-white border border-zinc-300 p-5 rounded-2xl shadow-md">
-            <h3 className="text-xl font-semibold mb-4">
-              Tailwind CSS
-            </h3>
-
-            <div className="flex gap-3">
-              <button className="flex-1 bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition">
-                View
-              </button>
-
-              <button className="flex-1 bg-yellow-500 text-white py-2 rounded-xl hover:bg-yellow-600 transition">
-                Edit
-              </button>
-
-              <button className="flex-1 bg-red-500 text-white py-2 rounded-xl hover:bg-red-600 transition">
-                Delete
-              </button>
-            </div>
-          </div>
-
-          {/* Note Card */}
-          <div className="bg-white border border-zinc-300 p-5 rounded-2xl shadow-md">
-            <h3 className="text-xl font-semibold mb-4">
-              JavaScript
-            </h3>
-
-            <div className="flex gap-3">
-              <button className="flex-1 bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition">
-                View
-              </button>
-
-              <button className="flex-1 bg-yellow-500 text-white py-2 rounded-xl hover:bg-yellow-600 transition">
-                Edit
-              </button>
-
-              <button className="flex-1 bg-red-500 text-white py-2 rounded-xl hover:bg-red-600 transition">
-                Delete
-              </button>
-            </div>
-          </div>
 
         </div>
       </main>
