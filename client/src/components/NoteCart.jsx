ddbd4198306b08e7f8668af5d393dd4d1a96d0fa
Milen03 <milen.atanasovv03@gmail.com>
2026-05-15 19:33:14 +0300

    Create NoteCart component

---
+++ client/src/components/NoteCart.jsx
@@ -0,0 +1,27 @@
+export default function(){
+
+    return(
+        <>
+        <div className="bg-white border border-zinc-300 p-5 rounded-2xl shadow-md">
+            <h3 className="text-xl font-semibold mb-4">
+              React Notes
+            </h3>
+
+            <div className="flex gap-3">
+              <button className="flex-1 bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition">
+                View
+              </button>
+
+              <button className="flex-1 bg-yellow-500 text-white py-2 rounded-xl hover:bg-yellow-600 transition">
+                Edit
+              </button>
+
+              <button className="flex-1 bg-red-500 text-white py-2 rounded-xl hover:bg-red-600 transition">
+                Delete
+              </button>
+            </div>
+          </div>
+
+        </>
+    )
+}
